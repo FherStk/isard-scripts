04acba9febf8c383b42f241f5d0fb21cac7f88d9
FherStk <porrino.fernando@gmail.com>
2022-10-20 03:57:59 +0200

    fix?

--- scripts/ubuntu-server-2204.sh
+++ scripts/ubuntu-server-2204.sh
@@ -2,9 +2,8 @@
 SCRIPT_VERSION="1.0.0"
 SCRIPT_NAME="Ubuntu Server 22.04 LTS (Default setup)"
 HOST_NAME="ubuntu-2204-server"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-echo $pwd
-
-source ./../utils/main.sh
+source $DIR/../utils/main.sh
 base-setup
 clear-and-reboot
