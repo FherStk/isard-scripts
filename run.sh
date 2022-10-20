e22866264fee967040951f88d27b0e274dfc31ed
FherStk <porrino.fernando@gmail.com>
2022-10-20 04:00:29 +0200

    sudo

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 SCRIPT_VERSION="1.0.0"
 SCRIPT_NAME="IsardVDI Script App"
 
+auto-update true `basename "$0"`
+check-sudo
+
 directory="./scripts"
 options=$(find $directory -mindepth 1 -maxdepth 1 -type f -not -name '*.exe' -printf "%f %TY-%Tm-%Td off\n");
 selected_files=$(dialog --radiolist "Pick files out of $directory" 60 70 25 $options --output-fd 1);
