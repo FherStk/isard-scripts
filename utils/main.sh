dbe3ce8a0b4248b3225d86d38c58e549f6b1fc9c
FherStk <porrino.fernando@gmail.com>
2022-10-20 15:49:37 +0200

    disabling kernel update message (reboot will be forced at the end)

--- utils/main.sh
+++ utils/main.sh
@@ -39,6 +39,7 @@ apt-upgrade()
     title "Upgrading the installed apps: "
     #Note: this is needed in order to disable interactive prompts like service-restart...
     sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
+    sed -i 's/#$nrconf{kernelhints} = -1;/$nrconf{kernelhints} = -1;/g' /etc/needrestart/needrestart.conf
 
     sudo apt update
     sudo apt upgrade -y
