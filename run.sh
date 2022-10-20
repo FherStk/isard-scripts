bb528971faba5b513c4da4065894c4fc265f3628
FherStk <porrino.fernando@gmail.com>
2022-10-20 10:52:08 +0200

    fix

--- run.sh
+++ run.sh
@@ -12,19 +12,19 @@ auto-update true `basename "$0"`
 check-sudo
 
 echo ""
-#trap 'clear' 0
+trap 'clear' 0
 
 FOLDER="$SCRIPT_PATH/scripts"
 OPTIONS=$(find $FOLDER -mindepth 1 -maxdepth 1 -type f -not -name '*.exe' -printf "%f %TY-%Tm-%Td off\n");
 SELECTED=$(dialog --title "${SCRIPT_NAME}" --radiolist "\nPick an IsardVDI script in order to install" 60 70 25 $OPTIONS --output-fd 1);
-#clear
+clear
 
 for f in $SELECTED
 do    
     echo ${AUTOSTART}
     echo ${PROFILE}
 
-    sed -i "s|'${AUTOSTART}'|'#${AUTOSTART}'|g" ${PROFILE}
+    sed -i "s|${AUTOSTART}|#${AUTOSTART}|g" ${PROFILE}
     source $SCRIPT_PATH/scripts/$f
 done
 
