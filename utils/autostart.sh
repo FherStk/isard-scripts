ae699118317068e9f976b79f9bafafede84dd706
FherStk <porrino.fernando@gmail.com>
2022-10-21 00:47:53 +0200

    simplifying

--- utils/autostart.sh
+++ utils/autostart.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-SCRIPT_VERSION="1.0.1"
-SCRIPT_NAME="Script Installer"
-
-
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $SCRIPT_PATH/../utils/main.sh
-
 
 trap : 0
-info "$SCRIPT_NAME" "$SCRIPT_VERSION"
-echo ""
-
 sudo $SCRIPT_PATH/../run.sh
 exit 0
