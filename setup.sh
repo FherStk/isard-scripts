11b0019906b7c89b5648bd9beaef528dcf45130d
FherStk <porrino.fernando@gmail.com>
2022-10-20 09:55:26 +0200

    fix for existing path

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ apt-req "dialog"
 
 echo ""
 title "Installing into /etc/isard-scripts:"
+rm -rf /etc/isard-scripts
 git clone https://github.com/FherStk/AutoCheck.git /etc/isard-scripts
 
 echo ""
