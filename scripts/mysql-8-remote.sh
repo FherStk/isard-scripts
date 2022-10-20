23e3578f1a1aeafa9f2bd9239bf458bfd918f009
FherStk <porrino.fernando@gmail.com>
2022-10-20 11:42:47 +0200

    testing

---
+++ scripts/mysql-8-remote.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCRIPT_VERSION="1.0.0"
+SCRIPT_NAME="Ubuntu Server 22.04 LTS (MySQL v8 - With remote connections)"
+HOST_NAME="mysql-server"
+
+SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $SCRIPT_PATH/../utils/main.sh
+
+base-setup
+apt-req "mysql-server-8.0"
+wget https://github.com/datacharmer/test_db/archive/refs/heads/master.zip
+unzip master.zip
+rm master.zip
+
+sudo -H -u root bash -c "mysql -t < ${PWD}/test_db-master/employees.sql"
+rm -R ${PWD}/test_db-master
+
+#sed -i "s|#bind-address|g" /etc/mysql/mysql.conf.d/mysqld.cnf 
+
+#sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf 
+
+clear-and-reboot
