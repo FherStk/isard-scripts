2c732e717544462a2a60173956541de3f1ebf330
FherStk <porrino.fernando@gmail.com>
2022-10-20 04:17:20 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPT_VERSION="1.0.0"
 SCRIPT_NAME="App Setup"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source ./utils/main.sh
 
@@ -16,14 +17,14 @@ apt-req "dialog"
 echo ""
 title "Setting up the isard-scripts-first-run service:"
 cp ./utils/isard-scripts-first-run.service /etc/systemd/system/isard-scripts-first-run.service
-sed -i "s|<PATH>|${HOME}|g" /etc/systemd/system/isard-scripts-first-run.service
+sed -i "s|<PATH>|${DIR}|g" /etc/systemd/system/isard-scripts-first-run.service
 systemctl daemon-reload
 sudo systemctl enable isard-scripts-first-run.service
 
 echo ""
 title "Setting up the isard-scripts-update service:"
 cp ./utils/isard-scripts-update.service /etc/systemd/system/isard-scripts-update.service
-sed -i "s|<PATH>|${HOME}|g" /etc/systemd/system/isard-scripts-update.service
+sed -i "s|<PATH>|${DIR}|g" /etc/systemd/system/isard-scripts-update.service
 systemctl daemon-reload
 sudo systemctl enable isard-scripts-update.service
 
