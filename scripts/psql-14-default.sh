a91a9acfd189eacbce7c9839f5b367f35efd9de5
FherStk <porrino.fernando@gmail.com>
2022-10-20 03:59:10 +0200

    fixes

--- scripts/psql-14-default.sh
+++ scripts/psql-14-default.sh
@@ -2,8 +2,9 @@
 SCRIPT_VERSION="1.0.0"
 SCRIPT_NAME="Ubuntu Server 22.04 LTS (PostgreSQL v14 - Default setup)"
 HOST_NAME="psql-server"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-source ./../utils/main.sh
+source $DIR/../utils/main.sh
 base-setup
 apt-req "postgresql-14"
 clear-and-reboot
