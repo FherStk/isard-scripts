da9d0e157a8f53abc090e75227df827b25fd4761
FherStk <porrino.fernando@gmail.com>
2022-10-21 00:43:30 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_VERSION="1.0.1"
+SCRIPT_VERSION="1.1.0"
 SCRIPT_NAME="Script Installer"
 
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -15,7 +15,7 @@ FOLDER="$SCRIPT_PATH/scripts"
 OPTIONS=$(find $FOLDER -mindepth 1 -maxdepth 1 -type f -name '*.sh' -printf "%f %TY-%Tm-%Td off\n");
 OPTIONS+=$(echo " NONE 1900-01-01 off")
 
-SELECTED=$(dialog --title "${SCRIPT_NAME}" --radiolist "\nPick an IsardVDI script in order to install" 60 70 25 $OPTIONS --output-fd 1);
+SELECTED=$(dialog --title "${SCRIPT_NAME} v${SCRIPT_VERSION}" --radiolist "\nPick an IsardVDI script in order to install" 60 70 25 $OPTIONS --output-fd 1);
 clear
 
 for f in $SELECTED
