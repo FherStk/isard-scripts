44f8ad0659e47fb649dd08d3de0753b3c2fa105a
FherStk <porrino.fernando@gmail.com>
2022-10-20 02:25:12 +0200

    changes

--- ubuntu-server-2204.sh
+++ ubuntu-server-2204.sh
@@ -3,6 +3,6 @@ SCRIPT_VERSION="1.0.0"
 SCRIPT_NAME="Ubuntu Server 22.04 LTS (Default setup)"
 HOST_NAME="ubuntu-2204-server"
 
-source ./main.sh
-setup-base
+source ./utils/main.sh
+base-setup
 clear-and-reboot
--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 SCRIPT_VERSION="1.0.0"
-SCRIPT_NAME="Update"
+SCRIPT_NAME="App Update"
 
-source ./main.sh
+source ./utils/main.sh
 
 trap 'abort' 0
 set -e
+
+info "$SCRIPT_NAME" "$SCRIPT_VERSION"
 auto-update false
+
+echo ""
 trap : 0
