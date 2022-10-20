ba725a4998812216bc49c3fe2fd57811dcd3b623
FherStk <porrino.fernando@gmail.com>
2022-10-20 02:45:23 +0200

    fixes

--- setup.sh
+++ setup.sh
@@ -8,8 +8,11 @@ trap 'abort' 0
 set -e
 
 info "$SCRIPT_NAME" "$SCRIPT_VERSION"
+
+echo ""
+title "Setting up the isard-scripts-update service:"
 cp ./utils/isard-scripts-update.service /etc/systemd/system/isard-scripts-update.service
-sed -i "s/'<PATH>'/'${pwd}'/g" /etc/systemd/system/isard-scripts-update.service
+sed -i "s|<PATH>|${HOME}|g" /etc/systemd/system/isard-scripts-update.service
 
 systemctl daemon-reload
 sudo systemctl enable isard-scripts-update.service
