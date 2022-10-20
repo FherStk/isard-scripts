c973b4042db125b4e6554188a2d913a66d17c7f2
FherStk <porrino.fernando@gmail.com>
2022-10-20 11:02:51 +0200

    test

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ FOLDER="$SCRIPT_PATH/scripts"
 OPTIONS=$(find $FOLDER -mindepth 1 -maxdepth 1 -type f -not -name '*.exe' -printf "%f %TY-%Tm-%Td off\n");
 OPTIONS+=("NONE" "1900-01-01" "off")
 echo ${OPTIONS[*]}
-#SELECTED=$(dialog --title "${SCRIPT_NAME}" --radiolist "\nPick an IsardVDI script in order to install" 60 70 25 $OPTIONS --output-fd 1);
+SELECTED=$(dialog --title "${SCRIPT_NAME}" --radiolist "\nPick an IsardVDI script in order to install" 60 70 25 $OPTIONS --output-fd 1);
 #clear
 
 for f in $SELECTED
