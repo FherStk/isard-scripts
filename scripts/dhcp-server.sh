2ee0cb5cd12127fa0c3bca016edca54972a8155f
FherStk <porrino.fernando@gmail.com>
2022-10-21 10:32:29 +0200

    hostname

--- scripts/dhcp-server.sh
+++ scripts/dhcp-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_VERSION="1.0.1"
 SCRIPT_NAME="Ubuntu Server 22.04 LTS (Default setup)"
-HOST_NAME="ubuntu-2204-server"
+HOST_NAME="dhcp-server"
 
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $SCRIPT_PATH/../utils/main.sh
