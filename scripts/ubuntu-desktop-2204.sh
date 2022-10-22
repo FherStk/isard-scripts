7b965d0ac5c9e3aa1e2c8da844311fcc1982377e
FherStk <porrino.fernando@gmail.com>
2022-10-22 21:23:08 +0200

    dhcp auto enabled on install, simplifies the logic :(

--- scripts/ubuntu-desktop-2204.sh
+++ scripts/ubuntu-desktop-2204.sh
@@ -16,4 +16,5 @@ gsettings set org.gnome.desktop.session idle-delay 0
 echo "Attaching favourite apps to the dash..."
 gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']"
 
+
 clear-and-reboot
