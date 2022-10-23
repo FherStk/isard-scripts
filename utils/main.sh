e7f0b1811d7f7f2d27a5880ba102d7b288b76fe9
FherStk <porrino.fernando@gmail.com>
2022-10-23 12:23:41 +0200

    test

--- utils/main.sh
+++ utils/main.sh
@@ -221,7 +221,7 @@ done-and-reboot(){
 clean()
 {
   echo "Clearing bash history..."
-  cat /dev/null > ~/.bash_history && history -c
+  run-in-user-session cat /dev/null > ~/.bash_history && history -c
 }
 
 run-in-user-session() {
