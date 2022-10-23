91075ab838c8ed6baa6b4db4226a725970764aa9
FherStk <porrino.fernando@gmail.com>
2022-10-23 11:28:21 +0200

    test

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ then
     cp $BASE_PATH/isard-scripts.desktop $DESKTOPFILE
     sed -i "s|<INSTALL_PATH>|$INSTALL_PATH|g" $DESKTOPFILE
     sed -i "s|<RUN_SCRIPT>|$RUN_SCRIPT|g" $DESKTOPFILE
-    sed -i "s|&|\\\\&|g" $DESKTOPFILE
+    #sed -i "s|&|\\\\&|g" $DESKTOPFILE
 else
     #Ubuntu Server
     echo "Setting up the $PROFILE entry..."
