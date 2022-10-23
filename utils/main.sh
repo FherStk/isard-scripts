f85dd636908231a042d5147745801c009d3c6d66
FherStk <porrino.fernando@gmail.com>
2022-10-23 16:23:08 +0200

    test

--- utils/main.sh
+++ utils/main.sh
@@ -137,7 +137,7 @@ set-address()
   echo ""
   echo "Setting up host address..."
 
-  _selected=$(dialog --nocancel --title "Network Configuration: enp3s0" --radiolist "\nSelect a configuration for the 'personal' network interface." 20 70 25 1 DHCP on 2 'Static IP address' off --output-fd 1);
+  _selected=$(dialog --nocancel --title "Network Configuration: enp3s0" --radiolist "\nSelect a configuration for the 'personal' network interface." 20 70 25 1 DHCP off 2 'Static IP address' on --output-fd 1);
   clear
   
   for f in $_selected
@@ -300,7 +300,7 @@ script-setup(){
     run-in-user-session gsettings set org.gnome.desktop.session idle-delay 0
 
     echo "Attaching favourite apps to the dash..."
-    run-in-user-session gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']"
+    run-in-user-session gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']"
 
   #else
     #Ubuntu Server   
