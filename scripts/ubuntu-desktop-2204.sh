6efc8096fbc9dfb00744e3a9776aaf96b493e0b4
FherStk <porrino.fernando@gmail.com>
2022-10-23 09:51:03 +0200

    test

--- scripts/ubuntu-desktop-2204.sh
+++ scripts/ubuntu-desktop-2204.sh
@@ -11,9 +11,10 @@ script-setup
 
 title "Setting up the desktop:"
 echo "Disabling the session timeout..."
-sudo -i -u $SUDO_USER bash -c "gsettings set org.gnome.desktop.session idle-delay 0"
+#sudo -i -u $SUDO_USER bash -c "gsettings set org.gnome.desktop.session idle-delay 0"
+run-in-user-session gsettings set org.gnome.desktop.session idle-delay 0
 
 echo "Attaching favourite apps to the dash..."
 sudo -i -u $SUDO_USER bash -c "gsettings set org.gnome.shell favorite-apps \"['firefox_firefox.desktop', 'thunderbird.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop']\""
 
-clear-and-reboot
+#clear-and-reboot
