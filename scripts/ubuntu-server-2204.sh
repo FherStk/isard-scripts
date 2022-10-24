f7008292fcf24b5f41d9c994cae5fb57f30be80f
FherStk <porrino.fernando@gmail.com>
2022-10-24 14:34:42 +0200

    test

--- scripts/ubuntu-server-2204.sh
+++ scripts/ubuntu-server-2204.sh
@@ -3,12 +3,10 @@ SCRIPT_VERSION="1.0.3"
 SCRIPT_NAME="Ubuntu Server 22.04 LTS (Default setup)"
 HOST_NAME="ubuntu-2204-server"
 
-echo "AAA"
-
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-echo "BBB"
 SCRIPT_FILE=$(basename $BASH_SOURCE)
-echo "CCC"
+
+echo $SCRIPT_PATH
 source $SCRIPT_PATH/../utils/main.sh
 echo "DDD"
 
