1b1caad75587bc72e3f1cfc18b4c6cf81b616f2b
FherStk <porrino.fernando@gmail.com>
2022-10-27 09:03:58 +0200

    testing

--- scripts/domjudge.sh
+++ scripts/domjudge.sh
@@ -18,5 +18,20 @@ sudo apt update
 apt-install "domjudge-judgehost-dbgsym"
 apt-install "domjudge-domserver"
 
+echo ""
+title "Setting up user permissions:"
+echo "Adding the current user to the domjudge group"
+usermod -a -G domjudge $SUDO_USER
+
+echo ""
+title "Setiing up the chroot:"
+dj_make_chroot
+
+echo ""
+title "Setiing up the grub entry:"
+sed -i "s|GRUB_CMDLINE_LINUX_DEFAULT=\"\"|GRUB_CMDLINE_LINUX_DEFAULT=\"quiet cgroup_enable=memory swapaccount=1\"|g" /etc/default/grub
+update-grub
+
+
 passwords-add "DOMjudge (http://<ip>/domjudge)" "admin" $(cat /etc/domjudge/initial_admin_password.secret)
 done-and-reboot
