b3f836cfe4820c8aca7ab44cd2c2b93fb232ec54
FherStk <porrino.fernando@gmail.com>
2022-10-28 10:00:51 +0200

    titles

--- scripts/pgadmin-v4.sh
+++ scripts/pgadmin-v4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-SCRIPT_VERSION="1.1.0"
-SCRIPT_NAME="Ubuntu Desktop 22.04 LTS (DBeaver Community Edition)"
-HOST_NAME="gantt-project"
+SCRIPT_VERSION="1.0.0"
+SCRIPT_NAME="Ubuntu Desktop 22.04 LTS (pgAdmin v4)"
+HOST_NAME="pgadmin"
 
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SCRIPT_FILE=$(basename $BASH_SOURCE)
