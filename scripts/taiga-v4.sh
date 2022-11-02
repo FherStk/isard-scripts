064c11f4301b864833093eaa621db44964e84f58
FherStk <porrino.fernando@gmail.com>
2022-11-02 23:47:18 +0100

    testing

--- scripts/taiga-v4.sh
+++ scripts/taiga-v4.sh
@@ -7,32 +7,32 @@ SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SCRIPT_FILE=$(basename $BASH_SOURCE)
 source $SCRIPT_PATH/../utils/main.sh
 
-# startup
-# script-setup
+startup
+script-setup
 
-# apt-install "docker"
-# apt-install "docker-compose"
-# cd /home/$SUDO_USER
+apt-install "docker"
+apt-install "docker-compose"
+cd /home/$SUDO_USER
 
-# echo ""
-# title "Downloading Taiga.io:"
-# git clone https://github.com/kaleidos-ventures/taiga-docker.git
-# cd taiga-docker/
-# git checkout stable
+echo ""
+title "Downloading Taiga.io:"
+git clone https://github.com/kaleidos-ventures/taiga-docker.git
+cd taiga-docker/
+git checkout stable
 
-# echo "Setting up the hostname..."
-get-interface
-echo $NETWORK_IFACE
+request-interface
+get-interface-address $INTERFACE
 
-# #TODO: select interface (show name and address)
-# sed -i "s|localhost|IP_ADDRESS|g" docker-compose.yml
+echo ""
+title "Setting up the environment:"
+echo "Setting up the hostname..."
+sed -i "s|localhost|$ADDRESS|g" docker-compose.yml
 
+echo "Setting up the environment..."
+docker-compose up -d
+#TODO: check the username and password setup with DJANGO vars
+docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage createsuperuser -e DJANGO_SUPERUSER_PASSWORD="taiga" DJANGO_SUPERUSER_USERNAME="taiga" DJANGO_SUPERUSER_EMAIL="taiga@taiga.com"
+docker-compose up -d
 
-# echo "Setting up the environment..."
-# docker-compose up -d
-# #TODO: check the username and password setup with DJANGO vars
-# docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage createsuperuser -e DJANGO_SUPERUSER_PASSWORD="taiga" DJANGO_SUPERUSER_USERNAME="taiga" DJANGO_SUPERUSER_EMAIL="taiga@taiga.com"
-# docker-compose up -d
-
-# passwords-add "Taiga.io (http://ip:9000)" "taiga" "taiga"
+passwords-add "Taiga.io (http://ip:9000)" "taiga" "taiga"
 # done-and-reboot
