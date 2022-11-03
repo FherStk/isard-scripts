42320c1a8c665f6ca2c235fe31ad72d85908c80d
FherStk <porrino.fernando@gmail.com>
2022-11-03 13:05:51 +0100

    testing

--- scripts/taiga-v4.sh
+++ scripts/taiga-v4.sh
@@ -31,20 +31,21 @@ sed -i "s|localhost|$ADDRESS|g" docker-compose.yml
 _user="taiga"
 echo "Setting up docker..."
 docker-compose up -d
+docker compose logs -f -t
 
 #TODO: this fails, must wait till up finished... should be executed after a reboot? 
 #docker compose logs -f -t {service_name1}
 #https://stackoverflow.com/questions/48783546/how-to-check-the-status-of-docker-compose-up-d-command
 #https://www.datanovia.com/en/lessons/docker-compose-wait-for-container-using-wait-tool/
 
-echo
-title "Setting up the superuser account:"
-echo "Creating the superuser account..."
-docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage createsuperuser --no-input --username $_user --email $_user@$_user.com
+# echo
+# title "Setting up the superuser account:"
+# echo "Creating the superuser account..."
+# docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage createsuperuser --no-input --username $_user --email $_user@$_user.com
 
-echo "Storing the superuser password..."
-docker-compose up -d
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='$_user'); u.set_password('$_user');u.save()" | docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage shell
+# echo "Storing the superuser password..."
+# docker-compose up -d
+# echo "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='$_user'); u.set_password('$_user');u.save()" | docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage shell
 
-passwords-add "Taiga.io (http://ip:9000)" "$_user" "$_user"
-#done-and-reboot
+# passwords-add "Taiga.io (http://ip:9000)" "$_user" "$_user"
+# #done-and-reboot
