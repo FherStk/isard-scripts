7d3db8f83979a57955e9b66269d556fed92616b9
FherStk <porrino.fernando@gmail.com>
2022-11-03 11:13:48 +0100

    automate setup does not work?

--- scripts/taiga-v4.sh
+++ scripts/taiga-v4.sh
@@ -32,8 +32,9 @@ _user="taiga"
 echo "Setting up docker..."
 docker-compose up -d
 docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage createsuperuser --no-input --username $_user --email $_user@$_user.com
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='$_user'); u.set_password('$_user');u.save()" | sudo docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage shell
+
 docker-compose up -d
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='$_user'); u.set_password('$_user');u.save()" | docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage shell
 
 passwords-add "Taiga.io (http://ip:9000)" "$_user" "$_user"
-done-and-reboot
+#done-and-reboot
