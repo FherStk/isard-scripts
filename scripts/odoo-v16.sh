5003febe4eeaa067652db4e8cea021d0ad4bfd39
FherStk <porrino.fernando@gmail.com>
2022-11-15 11:44:38 +0100

    removing files

--- scripts/odoo-v16.sh
+++ scripts/odoo-v16.sh
@@ -14,23 +14,29 @@ echo ""
 title "Setting up the Odoo repository:"
 wget -q -O - https://nightly.odoo.com/odoo.key | sudo gpg --dearmor -o /usr/share/keyrings/odoo-archive-keyring.gpg
 echo 'deb [signed-by=/usr/share/keyrings/odoo-archive-keyring.gpg] https://nightly.odoo.com/16.0/nightly/deb/ ./' | sudo tee /etc/apt/sources.list.d/odoo.list
-sudo apt-get update
+apt update
 
 echo ""
 title "Setting up the HTML to PDF component:"
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb -O wkhtmltox_0.12.5.deb
-sudo dpkg -i wkhtmltox_0.12.5.deb
+_file="xfonts-75dpi.deb"
+wget http://es.archive.ubuntu.com/ubuntu/pool/universe/x/xfonts-75dpi/xfonts-75dpi_1.0.4+nmu1.1_all.deb -O $_file
+dpkg -i $_file
+rm -f $_file
 
-wget http://es.archive.ubuntu.com/ubuntu/pool/universe/x/xfonts-75dpi/xfonts-75dpi_1.0.4+nmu1.1_all.deb -O xfonts-75dpi.deb
-sudo dpkg -i xfonts-75dpi.deb
+_file="libssl1.1.deb"
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb -O $_file
+dpkg -i $_file
+rm -f $_file
 
 apt-install "xfonts-base"
 apt-install "fontconfig"
 apt-install "libjpeg-turbo8"
 apt-install "libxrender1"
 
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb -O wkhtmltox_0.12.5.deb
-sudo dpkg -i wkhtmltox_0.12.5.deb
+_file="wkhtmltox_0.12.5.deb"
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb -O $_file
+dpkg -i $_file
+rm -f $_file
 
 passwords-add "PostgreSQL" "postgres" "N/A"
 passwords-add "Odoo (http://ip:8069)" "N/A" "N/A"
