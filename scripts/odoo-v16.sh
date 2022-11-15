e26f0c2b371105d7402d69c204ae9347efb66142
FherStk <porrino.fernando@gmail.com>
2022-11-15 12:19:29 +0100

    echo

--- scripts/odoo-v16.sh
+++ scripts/odoo-v16.sh
@@ -20,7 +20,7 @@ apt-install "odoo"
 apt-install "xfonts-base"
 
 echo ""
-title "Setting up the HTML to PDF component:"
+title "Setting up the HTML to PDF prerequisites:"
 _file="xfonts-75dpi.deb"
 wget http://es.archive.ubuntu.com/ubuntu/pool/universe/x/xfonts-75dpi/xfonts-75dpi_1.0.4+nmu1.1_all.deb -O $_file
 dpkg -i $_file
@@ -31,6 +31,8 @@ wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubu
 dpkg -i $_file
 rm -f $_file
 
+echo ""
+title "Setting up the HTML to PDF component:"
 _file="wkhtmltox_0.12.5.deb"
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox_0.12.5-1.focal_amd64.deb -O $_file
 dpkg -i $_file
