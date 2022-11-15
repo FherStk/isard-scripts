940781017de7b93771ee34f8907ec1ea12c30dbe
FherStk <porrino.fernando@gmail.com>
2022-11-15 17:29:42 +0100

    install within container

---
+++ scripts/odoo-v16-container.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCRIPT_VERSION="1.0.0"
+SCRIPT_NAME="Ubuntu Server 22.04 LTS (Odoo v16 - Within an LXC/LXD container)"
+HOST_NAME="odoo"
+
+SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPT_FILE=$(basename $BASH_SOURCE)
+source $SCRIPT_PATH/../utils/main.sh
+
+startup
+script-setup
+apt-install lxd
+
+_container="odoo-v16"
+_file="$_container/home/ubuntu/install.sh"
+lxc launch ubuntu:22.04 $_container
+lxc file push $SCRIPT_PATH/../utils/odoo-v16/install.sh $_file
+lxc exec $_container -- /bin/bash $_file
+
+passwords-add "PostgreSQL" "postgres" "N/A"
+passwords-add "Odoo (http://ip:8069)" "N/A" "N/A"
+done-and-reboot
