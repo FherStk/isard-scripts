2eade91df819597e52f5d37757d6bc67691b5f96
FherStk <porrino.fernando@gmail.com>
2022-11-16 16:58:35 +0100

    ready to test

--- scripts/odoo-v16-container.sh
+++ scripts/odoo-v16-container.sh
@@ -10,6 +10,9 @@ source $SCRIPT_PATH/../utils/main.sh
 startup
 script-setup
 apt-install lxc
+
+echo ""
+title "Setting up the LXC/LXD container:"
 lxd init --auto
 
 _container="odoo-v16"
@@ -18,20 +21,21 @@ lxc launch ubuntu:22.04 $_container
 lxc file push --recursive $SCRIPT_PATH/../ $_container$_path
 lxc exec $_container -- /bin/bash $_path/utils/odoo-v16/install.sh
 
-#TODO: en fer login, mostrar que cal connectar-se via SSH amb port forwarding per a accedir a Odoo
-#TODO: mirar si es pot fer una redirecció automàtica perquè no calgui fer la connexió SSH, sinó posar el port
-#8069 a la màquina d'isard i que aquesta l'envii cap al contenidor. Serà més fàcil per l'usuari.
-#`ssh -L 8069:$_addr:8069 $SUDO_USER@<ip-isard>`
+#WARNING:  The container requests the IP address using its MAC address as the identifier, so, in theory, should be the same on any scenario.
+#          If the container's IP address changes, the following lines should be invoked on startup.
+#          TIP: this also works -> ssh -L 8069:$_addr:8069 $SUDO_USER@<ip-isard>        
 
-#TODO: port forwarding => https://www.cyberciti.biz/faq/how-to-configure-ufw-to-forward-port-80443-to-internal-server-hosted-on-lan/
-#edit /etc/sysctl.conf
-#append net.ipv4.ip_forward=1
+echo ""
+title "Setting up port forwarding to the LXC/LXD container:"
 
-#sudo sysctl -p
-#sudo systemctl restart ufw
+#Source: https://www.cyberciti.biz/faq/how-to-configure-ufw-to-forward-port-80443-to-internal-server-hosted-on-lan/
+append-no-repeat "net.ipv4.ip_forward=1" "/etc/sysctl.conf"
 
-#_addr=$(lxc list "odoo-v16" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}')
-#iptables -t nat -A PREROUTING -p tcp --dport 8069 -j DNAT --to-destination $_addr:8069
+_addr=$(lxc list "odoo-v16" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}')
+iptables -t nat -A PREROUTING -p tcp --dport 8069 -j DNAT --to-destination $_addr:8069
+
+sysctl -p
+systemctl restart ufw
 
 passwords-add "PostgreSQL" "postgres" "N/A"
 passwords-add "Odoo (http://ip:8069)" "N/A" "N/A"
