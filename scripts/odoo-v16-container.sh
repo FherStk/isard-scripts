6f584c86382de3e62410b4b31eacc8f0b5ec7bed
FherStk <porrino.fernando@gmail.com>
2022-11-23 16:51:57 +0100

    echo

--- scripts/odoo-v16-container.sh
+++ scripts/odoo-v16-container.sh
@@ -20,34 +20,33 @@ _path="/home/ubuntu/"
 lxc launch ubuntu:22.04 $_container
 
 echo "TEST:"
-echo $SCRIPT_PATH/../
+echo $SCRIPT_PATH/..
 echo ${_container}${_path}
-echo $pwd
 
-lxc file push --recursive $SCRIPT_PATH/../ ${_container}${_path}
+lxc file push --recursive $SCRIPT_PATH/.. ${_container}${_path}
 lxc exec $_container -- /bin/bash ${_path}isard-scripts/utils/odoo-v16/install.sh
 
 # #WARNING:  The container requests the IP address using its MAC address as the identifier, so, in theory, should be the same on any scenario.
 # #          If the container's IP address changes, the following lines should be invoked on startup.
 # #          TIP: this also works -> ssh -L 8069:$_addr:8069 $SUDO_USER@<ip-isard>        
 
-# echo ""
-# title "Setting up port forwarding to the LXC/LXD container:"
+echo ""
+title "Setting up port forwarding to the LXC/LXD container:"
 
-# #Source: https://www.cyberciti.biz/faq/how-to-configure-ufw-to-forward-port-80443-to-internal-server-hosted-on-lan/
-# append-no-repeat "net.ipv4.ip_forward=1" "/etc/sysctl.conf"
+#Source: https://www.cyberciti.biz/faq/how-to-configure-ufw-to-forward-port-80443-to-internal-server-hosted-on-lan/
+append-no-repeat "net.ipv4.ip_forward=1" "/etc/sysctl.conf"
 
-# _addr=$(lxc list "odoo-v16" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}')
-# iptables -t nat -A PREROUTING -p tcp --dport 8069 -j DNAT --to-destination $_addr:8069
+_addr=$(lxc list "odoo-v16" -c 4 | awk '!/IPV4/{ if ( $2 != "" ) print $2}')
+iptables -t nat -A PREROUTING -p tcp --dport 8069 -j DNAT --to-destination $_addr:8069
 
-# sysctl -p
-# systemctl restart ufw
+sysctl -p
+systemctl restart ufw
 
-# echo ""
-# title "Performing the initial LXC/LXD snapshot:"
-# lxc snapshot $_container initial
-# echo "Done"
+echo ""
+title "Performing the initial LXC/LXD snapshot:"
+lxc snapshot $_container initial
+echo "Done"
 
-# passwords-add "PostgreSQL" "postgres" "N/A"
-# passwords-add "Odoo (http://ip:8069)" "N/A" "N/A"
-# done-and-reboot
+passwords-add "PostgreSQL" "postgres" "N/A"
+passwords-add "Odoo (http://ip:8069)" "N/A" "N/A"
+done-and-reboot
