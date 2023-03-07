0871835a815ac89d64ef0b412e66a47f6d78c539
FherStk <porrino.fernando@gmail.com>
2023-03-07 09:39:24 +0100

    removed unnecessary 'sudo' because any script should run as sudo

---
+++ scripts/open-project-v12.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+SCRIPT_VERSION="1.1.0"
+SCRIPT_NAME="Ubuntu Desktop 22.04 LTS (GanttProject v3)"
+HOST_NAME="gantt-project"
+
+SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SCRIPT_FILE=$(basename $BASH_SOURCE)
+source $SCRIPT_PATH/../utils/main.sh
+
+startup
+script-setup
+
+echo ""
+title "Downloading OpenProject's public key:"
+wget -qO- https://dl.packager.io/srv/opf/openproject/key | sudo apt-key add -
+
+
+echo ""
+title "Adding the repository:"
+sudo wget -O /etc/apt/sources.list.d/openproject.list https://dl.packager.io/srv/opf/openproject/stable/12/installer/ubuntu/22.04.repo
+
+sudo apt update    
+
+
+
+
+
+
+apt-install "default-jre"
+
+echo ""
+title "Downloading GanttProject:"
+wget https://www.ganttproject.biz/dl/3.2.3240/lin -O ganttproject.deb
+
+echo ""
+title "Installing GanttProject:"
+dpkg -i ganttproject.deb
+run-in-user-session gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'ganttproject.desktop']"
+
+echo ""
+title "Cleaning:"
+echo "Removing downloaded data..."
+rm -f ganttproject.deb
+
+done-and-reboot
