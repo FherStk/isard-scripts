7bc818987411350a1a3c9b634d6105e34abeb61d
FherStk <porrino.fernando@gmail.com>
2023-03-21 15:42:11 +0100

    Fix for taiga .env file changes

--- scripts/taiga-v6.sh
+++ scripts/taiga-v6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_VERSION="1.0.0"
+SCRIPT_VERSION="1.1.0"
 SCRIPT_NAME="Ubuntu Server 22.04 LTS (Taiga.io v6)"
 HOST_NAME="taiga"
 
@@ -26,7 +26,7 @@ git checkout stable
 echo ""
 title "Setting up the environment:"
 echo "Setting up the hostname..."
-sed -i "s|localhost|$ADDRESS|g" docker-compose.yml
+sed -i "s|localhost|$ADDRESS|g" .env
 
 _user="taiga"
 echo "Setting up docker..."
