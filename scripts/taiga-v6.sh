fe5b201eb07c94d3b2af9b07feaf2105f48160f9
FherStk <porrino.fernando@gmail.com>
2023-05-31 11:26:45 +0200

    test

--- scripts/taiga-v6.sh
+++ scripts/taiga-v6.sh
@@ -7,60 +7,61 @@ SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SCRIPT_FILE=$(basename $BASH_SOURCE)
 source $SCRIPT_PATH/../utils/main.sh
 
-startup
-script-setup
+#startup
+#script-setup
 
 request-interface "Taiga Domain" "Select the network interface where Taiga will be listening to:"
 get-interface-address $INTERFACE
 
-apt-install "docker"
-apt-install "docker-compose"
 
-_path = "/etc/taiga"
-cd /home/$SUDO_USER
+# apt-install "docker"
+# apt-install "docker-compose"
 
-echo ""
-title "Downloading Taiga.io:"
-git clone https://github.com/kaleidos-ventures/taiga-docker.git
-mv taiga-docker $_path
-cd $_path
-git checkout stable
+# _path = "/etc/taiga"
+# cd /home/$SUDO_USER
 
-echo ""
-title "Setting up the environment:"
-echo "Setting up the hostname..."
-sed -i "s|localhost|$ADDRESS|g" .env
+# echo ""
+# title "Downloading Taiga.io:"
+# git clone https://github.com/kaleidos-ventures/taiga-docker.git
+# mv taiga-docker $_path
+# cd $_path
+# git checkout stable
 
-_user="taiga"
-echo "Setting up docker..."
-systemctl enable docker
-docker-compose up -d
+# echo ""
+# title "Setting up the environment:"
+# echo "Setting up the hostname..."
+# sed -i "s|localhost|$ADDRESS|g" .env
 
-echo "Waiting for taiga services to startup..."
-while [ $(docker-compose logs | grep -c "Listening at: http://0.0.0.0:8000") -eq 0 ];
-do    
-    sleep 1
-done
-echo "Taiga services are ready..."
+# _user="taiga"
+# echo "Setting up docker..."
+# systemctl enable docker
+# docker-compose up -d
 
-echo
-title "Setting up the superuser account:"
-echo "Creating the superuser account..."
-docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage createsuperuser --no-input --username $_user --email $_user@$_user.com
+# echo "Waiting for taiga services to startup..."
+# while [ $(docker-compose logs | grep -c "Listening at: http://0.0.0.0:8000") -eq 0 ];
+# do    
+#     sleep 1
+# done
+# echo "Taiga services are ready..."
 
-echo "Storing the superuser password..."
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='$_user'); u.set_password('$_user');u.save()" | docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage shell
+# echo
+# title "Setting up the superuser account:"
+# echo "Creating the superuser account..."
+# docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage createsuperuser --no-input --username $_user --email $_user@$_user.com
 
-echo "Creating the startup service..."
-_service="/etc/systemd/system/taiga.service"
-cp $SCRIPT_PATH/../utils/taiga/taiga.service $_service
-sed -i "s|<user>|root|g" $_service
-sed -i "s|<path>|$_path|g" $_service
-sed -i "s|<file>|launch-taiga.sh|g" $_service
+# echo "Storing the superuser password..."
+# echo "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='$_user'); u.set_password('$_user');u.save()" | docker-compose -f docker-compose.yml -f docker-compose-inits.yml run --rm taiga-manage shell
 
-echo "Enabling the startup service..."
-systemctl enable taiga
-systemctl start taiga
+# echo "Creating the startup service..."
+# _service="/etc/systemd/system/taiga.service"
+# cp $SCRIPT_PATH/../utils/taiga/taiga.service $_service
+# sed -i "s|<user>|root|g" $_service
+# sed -i "s|<path>|$_path|g" $_service
+# sed -i "s|<file>|launch-taiga.sh|g" $_service
 
-passwords-add "Taiga.io (http://ip:9000)" "$_user" "$_user"
-done-and-reboot
+# echo "Enabling the startup service..."
+# systemctl enable taiga
+# systemctl start taiga
+
+# passwords-add "Taiga.io (http://ip:9000)" "$_user" "$_user"
+# done-and-reboot
