1f84f0d1d327bb057539e148a4a708004dcb4b5b
FherStk <porrino.fernando@gmail.com>
2024-07-31 13:53:42 +0200

    testing server

--- ubuntu-24.04/scripts/ubuntu-server-2404.sh
+++ ubuntu-24.04/scripts/ubuntu-server-2404.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_VERSION="1.0.0"
-SCRIPT_NAME="Ubuntu Desktop 24.04 LTS (Default setup)"
-HOST_NAME="ubuntu-desktop-2404"
+SCRIPT_NAME="Ubuntu Server 24.04 LTS (Default setup)"
+HOST_NAME="ubuntu-server-2404"
 
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SCRIPT_FILE=$(basename $BASH_SOURCE)
