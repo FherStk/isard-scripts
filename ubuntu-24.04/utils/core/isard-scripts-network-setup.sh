3a9976d964071f33d86bb225a9ca0fe80dc57112
FherStk <porrino.fernando@gmail.com>
2024-07-31 16:06:04 +0200

    Fixed the wired network up

--- ubuntu-24.04/utils/core/isard-scripts-network-setup.sh
+++ ubuntu-24.04/utils/core/isard-scripts-network-setup.sh
@@ -9,9 +9,9 @@ ip link set enp2s0 name Personal1
 ip link set enp3s0 name WireguardVPN
 ip link set enp4s0 name GroupNetwork1
 
-ip link set enp1s0 up
-ip link set enp2s0 up
-ip link set enp3s0 up
-ip link set enp4s0 up
+ip link set Default up
+ip link set Personal1 up
+ip link set WireguardVPN up
+ip link set GroupNetwork1 up
 
 netplan apply
