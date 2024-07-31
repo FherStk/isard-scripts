0cf87c749f7561c6d8a7dad60d4e636ceb037251
FherStk <porrino.fernando@gmail.com>
2024-07-31 15:45:39 +0200

    fixing desktop enp2s0 static address

--- ubuntu-24.04/utils/core/isard-scripts-network-setup.sh
+++ ubuntu-24.04/utils/core/isard-scripts-network-setup.sh
@@ -9,9 +9,9 @@ ip link set enp2s0 name Personal1
 ip link set enp3s0 name WireguardVPN
 ip link set enp4s0 name GroupNetwork1
 
-netplan apply
-
 ip link set enp1s0 up
 ip link set enp2s0 up
 ip link set enp3s0 up
 ip link set enp4s0 up
+
+netplan apply
