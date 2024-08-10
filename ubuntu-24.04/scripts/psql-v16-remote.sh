4930c036ae3141eaa12ffc82098c462ed8684611
FherStk <porrino.fernando@gmail.com>
2024-08-10 11:27:01 +0200

    Fixinv v14 to v16

--- ubuntu-24.04/scripts/psql-v16-remote.sh
+++ ubuntu-24.04/scripts/psql-v16-remote.sh
@@ -18,9 +18,9 @@ echo "Preparing the localhost 'postgres' user..."
 sudo -i -u postgres bash -c "psql -c \"ALTER USER postgres WITH PASSWORD 'postgres';\""
 
 echo "Opening the binding address to '*'..."
-sed -i "s|#listen_addresses = 'localhost'|listen_addresses = '*'|g" /etc/postgresql/14/core/postgresql.conf
+sed -i "s|#listen_addresses = 'localhost'|listen_addresses = '*'|g" /etc/postgresql/16/core/postgresql.conf
 
-_pghba_file="/etc/postgresql/14/core/pg_hba.conf"
+_pghba_file="/etc/postgresql/16/core/pg_hba.conf"
 _pghba_line1="host	all		all		192.168.1.1/16		md5" #personal
 _pghba_line2="host	all		all		10.0.0.1/8		    md5" #vpn
 
@@ -29,11 +29,12 @@ append-no-repeat "$_pghba_line2" "$_pghba_file"
 
 service postgresql restart
 
-echo ""
-title "Setting up the demo database:"
-wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip -O dvdrental.zip
-unzip dvdrental.zip
-rm -f dvdrental.zip
+#TODO: request if populate or not the demo BBDD
+# echo ""
+# title "Setting up the demo database:"
+# wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip -O dvdrental.zip
+# unzip dvdrental.zip
+# rm -f dvdrental.zip
 
 PGPASSWORD=postgres psql -U postgres -h localhost -c "CREATE DATABASE dvdrental;"
 PGPASSWORD=postgres pg_restore -U postgres -h localhost -d dvdrental dvdrental.tar;
